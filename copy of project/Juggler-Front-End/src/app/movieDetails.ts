26639c16e0db449a232cf31aeaec33cc6648a654
Likitha M P <mplikitha@gmail.com>
2018-09-27 13:28:44 +0530

    documents

--- copy of project/Juggler-Front-End/src/app/movieDetails.ts
+++
@@ -1,7 +0,0 @@
-export class MovieDetails {
-      public name: string;
-      public moviePoster: string;
-      public moviegenre: string;
-      public movielanguage: string;
-      public moviedescription: string;
-  }
